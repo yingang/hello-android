2e5da3239418e5e9ac193ba08da09d77208346a7
Yin Gang <zenith.yin@gmail.com>
2015-06-21 09:57:22 +0800

    finish chapter 14.4

---
+++ CoolWeather/src/com/yingang/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,6 @@
+package com.yingang.coolweather.util;
+
+public interface HttpCallbackListener {
+	void onFinish(String response);
+	void onError(Exception e);
+}
---
+++ CoolWeather/src/com/yingang/coolweather/util/HttpUtil.java
@@ -0,0 +1,47 @@
+package com.yingang.coolweather.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+	
+	public static void sendHttpRequest(final String address, final HttpCallbackListener listener) {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				HttpURLConnection conn = null;
+				URL url;
+				try {
+					url = new URL(address);
+					conn = (HttpURLConnection) url.openConnection();
+					conn.setRequestMethod("GET");
+					conn.setConnectTimeout(8000);
+					conn.setReadTimeout(8000);
+					InputStream in = conn.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+					StringBuilder resp = new StringBuilder();
+					String line;
+					while ((line = reader.readLine()) != null) {
+						resp.append(line);
+					}
+					if (listener != null) {
+						listener.onFinish(resp.toString());
+					}
+				} catch (Exception e) {
+					if (listener != null) {
+						listener.onError(e);
+					}
+				} finally {
+					if (conn != null) {
+						conn.disconnect();
+					}
+				}
+			}
+			
+		}).start();
+	}
+}
